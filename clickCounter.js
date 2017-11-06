a625641db32252041e0ee4557115451f8a62eee8
BIT Student <classroom@bgit.rs>
2017-11-06 14:44:04 +0100

    New repo

---
+++ clickCounter.js
@@ -0,0 +1,18 @@
+let cnt = 0;
+
+let button = document.getElementById('dugme');
+button.addEventListener('click', increment);
+
+function increment() {
+    let counter = sessionStorage.getItem('counter'); 
+    console.log(counter);
+    if (counter !== 'undefined') {
+        cnt = counter;
+    }
+
+    cnt++;
+
+    sessionStorage.setItem('counter', cnt);
+
+    button.textContent = cnt;
+}
